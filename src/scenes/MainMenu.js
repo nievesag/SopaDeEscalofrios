557eb8f0053f83e8856f0654cd9b228f0a8bc875
acostacion <carmengomezb27@gmail.com>
2024-10-14 19:29:46 +0200

    estrepitoso fallo por cansancio en el intento de hacer un menu

---
+++ src/scenes/MainMenu.js
@@ -0,0 +1,16 @@
+export default class MainMenu extends Phaser.Scene {
+	constructor() {
+		super({ key: 'MainMenu'});
+	}
+
+	preload () {
+		this.load.image("mainMenu", "./assets/images/mainMenu.jpg");
+		this.load.image("startButton", "./assets/images/startButton.jpg");
+		this.load.audio("f3ale", "./assets/images/f3ale.mp3");
+	}
+
+	create (){
+		this.add.image(0, 0, "mainMenu").setOrigin(0);
+
+	}
+  }
