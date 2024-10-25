ea57578c493255321a40870ef8a2fc2725fe72c6
nievesag <nievalon@ucm.es>
2024-10-25 18:49:07 +0200

    x2

--- src/scenes/Game1.js
+++ src/scenes/Game1.js
@@ -10,7 +10,44 @@ export default class Game1 extends Phaser.Scene {
     
     }
     
-    create (){
-        let boxes = this.physics.add
+    create () {
+        let boxes = this.physics.add.group(); // grupo de fisicas para las cajas
+
+        // ---- Objectos de escena ----
+        let playerG1 = new PlayerG1(this, 50, 50);
+        let box1 = new Box(this, 200, 0, boxes);
+        let box2 = new Box(this, 400, 0, boxes);
+
+        playerG1.bodyOffset.onCollide = true; // para poder colisionar
+
+        let scene = this; // referencia a la escena
+
+        this.physics.add.collider(playerG1, boxes); // colisionan cajas y jugador
+
+        scene.physics.world.on('collide', function(gameObject1, gameObject2, body1, body2) {
+			// colision del player con una caja
+			if(gameObject1 === playerG1 && boxes.contains(gameObject2)) {
+				if(gameObject1.isAttackInProcess()) {
+					gameObject2.destroyMe()
+				}
+			}
+		});	
     }
+
+    init() {
+
+        // para llevar el control de limite de tiempo
+        this.maxTime = document.getElementById('targetTime');
+
+        this.time = 0; 
     }
+
+    update(time, dt) {
+
+        // ---- limite de tiempo ----
+        this.time += dt;
+        if(this.time > this.maxTime.value*1000) {
+            
+        }
+    }
+}
