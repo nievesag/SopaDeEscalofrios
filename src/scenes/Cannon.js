5e8b633482367d0ebc6e0d0b7ffd32b53fed1884
acostacion <carmengomezb27@gmail.com>
2024-10-27 17:46:55 +0100

    hola

--- src/scenes/Cannon.js
+++ src/scenes/Cannon.js
@@ -2,13 +2,6 @@ export default class Cannon extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y) {
       super(scene, x, y, 'cannon');
 
-<<<<<<< Updated upstream
-      let posX, posY;
-      let dir;
-      let force;
-      let h, w;
-      let angle = 0;
-=======
       this.posX, posY;
       this.dir;
       this.force;
@@ -17,14 +10,10 @@ export default class Cannon extends Phaser.GameObjects.Sprite {
 
       // Añadimos el cañón a la escena.
       this.scene.add.existing(this);
->>>>>>> Stashed changes
     }
 
     create(){
       const cannon = this.add.image(130, 464, 'cannon').setDepth(1);
-<<<<<<< Updated upstream
-=======
       this.scene.add.existing(this);
->>>>>>> Stashed changes
     }
   }
