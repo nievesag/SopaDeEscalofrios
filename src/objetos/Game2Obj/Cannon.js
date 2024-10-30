ce9ea87118d5b604b3601baae772e703ae4e26b2
acostacion <carmengomezb27@gmail.com>
2024-10-30 18:26:51 +0100

    en dios ya no me cago

--- src/objetos/Game2Obj/Cannon.js
+++ src/objetos/Game2Obj/Cannon.js
@@ -1,4 +1,4 @@
-class Cannon extends Phaser.GameObjects.Sprite {
+ export default class Cannon extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y) {
       super(scene, x, y, 'cannon');
 
--- src/scenes/Game2.js
+++ src/scenes/Game2.js
@@ -6,10 +6,61 @@ export default class Game2 extends Phaser.Scene {
     }
     
     preload () {
-        this.load.image('cannon', '../assets/images/cannon.jpg')
+        // Cannon.
+        this.load.image('cannonBody', '../assets/images/cannonBody.jpg');
+        this.load.image('cannonHead', '../assets/images/cannonHead.png');
+
+        // Bg.
+        this.load.image('background', '../assets/images/rionilo.jpg');
     }
     
+    // https://phaser.io/examples/v3.85.0/physics/arcade/view/velocity-from-angle
     create (){
-    
+
+        // BACKGROUND.
+        this.make.sprite({
+            x: 510,
+            y: 375, 
+            key: 'background',
+            scale : {
+                x: 0.9,
+                y: 1.1
+            },
+        })
+
+        // --- CANNON ---.
+
+
+        const cannonBody = this.make.image({ // Cannon Body.
+            x: 150,
+            y: 625, 
+            key: 'cannonBody',
+            scale : {
+                x: 0.5,
+                y: 0.5
+            },
+        }).setDepth(1);
+
+        const cannonHead = this.make.image({ // Cannon Head.
+            x: 300,
+            y: 475, 
+            angle: 0,
+            key: 'cannonHead',
+            flipY: true,
+            scale : {
+                x: 0.25,
+                y: 0.30
+            },
+        }).setDepth(2);
+
+        let angle = 0;
+
+        this.input.on('pointermove', (pointer) =>
+            {
+                angle = Phaser.Math.Angle.BetweenPoints(cannonBody, pointer);
+                cannonHead.rotation = angle + 30;
+            });
+
+
     }
     }
