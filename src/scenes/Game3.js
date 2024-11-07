147ad4d8747e08709dca51d239b4e83113f7e6cf
M00n-Hun7r3ss <denisaju@ucm.es>
2024-11-07 14:19:22 +0100

    BUEEENO es un comienzo

--- src/scenes/Game3.js
+++ src/scenes/Game3.js
@@ -8,6 +8,7 @@ export default class Game3 extends Phaser.Scene {
     constructor() {
         super({ key: 'Game3'});
 
+        //Colores de los bichos
         const COLOR = {
             RED: 'RED',
             ORANGE: 'ORANGE',
@@ -23,10 +24,18 @@ export default class Game3 extends Phaser.Scene {
     preload () {
         //Player (place holder)
         this.load.image('cuadrado', '../assets/images/icon500.jpg');
+        this.load.image('cannonHead', '../assets/images/Burbujas.png');
+
+        // A lanzar
+        this.load.image('shootingBeetle', '../assets/images/BurbujaRoja.png')
     }
     
+    // https://phaser.io/examples/v3.85.0/physics/arcade/view/velocity-from-angle
+
     create (){
-        this.make.sprite({
+
+        // --- CANNON ---.
+        const cannon = this.make.image({ // Cannon Body.
             x: 500,
             y: 750, 
             key: 'cuadrado',
@@ -34,8 +43,53 @@ export default class Game3 extends Phaser.Scene {
                 x: 0.25,
                 y: 0.25
             },
-        })
+        }).setDepth(1);
 
-        this.pointer = this.scene.input.activePointer;
+        const cannonHead = this.make.image({ // Cannon Head.
+            x: 500,
+            y: 650, 
+            angle: 90,
+            key: 'cannonHead',
+            //flipY: true,
+            scale : {
+                x: 0.3,
+                y: 0.3,
+            },
+        }).setDepth(2);
+
+        // --- VESSEL ---. EN UN FUTURO SERÁ SPRITESHEET
+        const shootingBeetle = this.physics.add.image(cannon.x, cannon.y - 50, 'shootingBeetle').setScale(1); // Añade la vasija en la pos del cañón.
+        shootingBeetle.disableBody(true, true); // Desactiva la vessel para que no colisione ni se vea todavía.
+        
+        shootingBeetle.setCollideWorldBounds(true); // Para que no se salga de los límites del mundo.
+        //vessel.setDrag(100); // Fricción con el suelo.               
+
+        // Dibuja la línea de la dir.
+        const graphics = this.add.graphics({ lineStyle: { width: 10, color: 0x6714d8 , alpha: 0.5 } });
+        const line = new Phaser.Geom.Line(); 
+
+        let angle = 0; // Inicializa el ángulo a 0.
+
+        // SIGUE AL MOUSE.
+        this.input.on('pointermove', (pointer) =>
+            {
+                angle = Phaser.Math.Angle.BetweenPoints(cannon, pointer); // Ángulo cañón -> mouse.
+                cannonHead.rotation = angle + 30; // Pone la rotación del cañón mirando al mouse (con unos ajustes).
+
+                // Línea gráfica de la dir.
+                Phaser.Geom.Line.SetToAngle(line, cannonHead.x, cannonHead.y, angle+0.15, 128); 
+                graphics.clear().strokeLineShape(line); // Limpia y redibuja la línea.
+            });
+
+        // AL HACER CLIC.
+        this.input.on('pointerup', () =>
+            {
+                shootingBeetle.enableBody(true, cannonHead.x, cannonHead.y, true, true); // Activa la vessel y la pone donde cannonHead.
+                //chick.play('fly'); // animación de vuelo del pollo.
+                this.physics.velocityFromRotation(angle, 600, shootingBeetle.body.velocity); // Lanza a la vasija con un ángulo y velocidad.
+                
+            });
+
+
     }
-    }
+}
