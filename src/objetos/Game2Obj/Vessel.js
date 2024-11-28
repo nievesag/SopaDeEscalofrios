7006876f8d64556f4ff47cb47b1793fa70e41974
acostacion <carmengomezb27@gmail.com>
2024-11-28 16:00:21 +0100

    PUNCH

--- src/objetos/Game2Obj/Vessel.js
+++ src/objetos/Game2Obj/Vessel.js
@@ -1,41 +1,60 @@
 export default class Vessel extends Phaser.GameObjects.Image{
     constructor(scene, cannon, maelstromObs){
-        super(scene, cannon.cannonBody.x, cannon.cannonBody.y-50, 'vessel')
-        this.setScale(0.2);
-        scene.add.existing(this);
-        scene.physics.add.existing(this);
-        //crocodileObs, hippoObs){
+        super(scene, cannon.cannonBody.x, cannon.cannonBody.y - 50, 'vessel')
+
         this.scene = scene;
         this.cannon = cannon;
         this.maelstromObs = maelstromObs;
-        //this.crocodileObs = crocodileObs;
-        //this.hippoObs = hippoObs;
 
-        // --- VESSEL ---. EN UN FUTURO SERÁ SPRITESHEET
-        //this.vessel = this.scene.physics.add.image(this.cannon.cannonBody.x, this.cannon.cannonBody.y - 50, 'vessel').setScale(0.2); // Añade la vasija en la pos del cañón.
+        // Añadir a esta escena.
+        scene.add.existing(this); 
 
-        // disableBody([disableGameObject], [hideGameObject]).
-        this.disableBody(  true , true); // Desactiva la vessel para que no colisione ni se vea todavía.
-        this.setCollideWorldBounds(true); // Para que no se salga de los límites del mundo.
-        this.setDrag(100); // Fricción con el suelo.
+        // Le pone físicas a la vasija.
+        scene.physics.add.existing(this);
 
+        // Le pone el tamaño.
+        this.setScale(0.2);
+
+        // Configuración de las físicas.
+        this.body.setCollideWorldBounds(true); // Para que no se salga de los límites del mundo.
+        this.body.setDrag(100); // Fricción con el suelo.
+
+        // Al comienzo se desactiva.
+        this.disableBody(true , true); 
+        
         // La cámara sigue al vessel.
         this.scene.cameras.main.startFollow(this, false, 0.2, 0.2); 
+
+        /*//crocodileObs, hippoObs){
+        //this.crocodileObs = crocodileObs;
+        //this.hippoObs = hippoObs;
+        // --- VESSEL ---. EN UN FUTURO SERÁ SPRITESHEET
+        //this.vessel = this.scene.physics.add.image(this.cannon.cannonBody.x, this.cannon.cannonBody.y - 50, 'vessel').setScale(0.2); // Añade la vasija en la pos del cañón.
+        // disableBody([disableGameObject], [hideGameObject]).*/
     }
 
     launchVessel(angle){
-        //this.enableBody(true, this.cannon.cannonHead.x, this.cannon.cannonHead.y, true, true); // Activa la vessel y la pone donde cannonHead.
+        // Activa la vessel y la pone donde cannonHead.
+        this.enableBody(true, this.cannon.cannonHead.x, this.cannon.cannonHead.y, true, true); 
+
+        // Lanza a la vasija con un ángulo y velocidad.
+        this.scene.physics.velocityFromRotation(angle, 600, this.body.velocity); 
+
         //chick.play('fly'); // animación de vuelo del pollo.
-        this.scene.physics.velocityFromRotation(angle, 600, this.body.velocity); // Lanza a la vasija con un ángulo y velocidad.
     }
 
     collisionWithVessel(obstacle){
 
+        // NOTA:
         // OVERLAP SI SUPERPONEN
         // COLlIDE SI COL.
+
+        // Si colisiona la vasija con el obstáculo...
         if(this.scene.physics.world.collide(this, obstacle)){
-            if(obstacle === this.maelstromObs){
-                this.destroy;
+
+            // OBSTÁCULO:
+            if(obstacle === this.maelstromObs){ // MAELSTROM.
+                this.destroy();
             }
             else if(obstacle === this.crocodileObs){
                 // Lo que pasa cuando colisiona con CROCODILE.
